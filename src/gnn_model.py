0040fb4ae50a33ee67557ae5b9718782ebd2e878
zehowrld <hiteshnegi08@gmail.com>
2026-01-28 18:49:38 +0530

    model py update

--- src/gnn_model.py
+++ src/gnn_model.py
@@ -1,14 +1,17 @@
 import torch
+import torch.nn.functional as F 
 from torch_geometric.nn import SAGEConv
 
 class FraudGNN(torch.nn.module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels):
         super(FraudGNN, self).__init__()
-        # Layers that learn from neighboring account behavior
-        self.conv1 = SAGEConv(in_channels, 64)
-        self.conv2 = SAGEConv(64, out_channels)
+        # SAGEConv 'looks' at the neighbors to find fraud rings
+        self.conv1 = SAGEConv(in_channels, 16)
+        self.conv2 = SAGEConv(16, 1)
 
     def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index).relu()
+        # x = node features, edge_index = connections
+        x = self.conv1(x, edge_index)
+        x = F.relu()
         x = self.conv2(x, edge_index)
-        return torch.sigmoid(x) # Returns fraud probability
+        return torch.sigmoid(x) # Returns probability of fraud (0 to 1)
